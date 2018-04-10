a76b333b509003cd27553f656ccec855e3f36d5b
m <m@bitsnbites.eu>
2018-04-10 07:58:56 +0200

    Catch more exceptions at the top level

--- src/main.cpp
+++ src/main.cpp
@@ -17,11 +17,12 @@
 //  3. This notice may not be removed or altered from any source distribution.
 //--------------------------------------------------------------------------------------------------
 
-#include "string_list.hpp"
 #include "cache.hpp"
 #include "compiler_wrapper.hpp"
+#include "debug_utils.hpp"
 #include "gcc_wrapper.hpp"
 #include "ghs_wrapper.hpp"
+#include "string_list.hpp"
 #include "sys_utils.hpp"
 
 #include <iostream>
@@ -30,15 +31,33 @@
 
 namespace {
 [[noreturn]] void clear_cache_and_exit() {
-  bcache::cache_t cache;
-  cache.clear();
-  std::exit(0);
+  int return_code = 0;
+  try {
+    bcache::cache_t cache;
+    cache.clear();
+  } catch (const std::exception& e) {
+    std::cerr << "*** Unexpected error: " << e.what() << "\n";
+    return_code = 1;
+  } catch (...) {
+    std::cerr << "*** Unexpected error.\n";
+    return_code = 1;
+  }
+  std::exit(return_code);
 }
 
 [[noreturn]] void show_stats_and_exit() {
-  bcache::cache_t cache;
-  cache.show_stats();
-  std::exit(0);
+  int return_code = 0;
+  try {
+    bcache::cache_t cache;
+    cache.show_stats();
+  } catch (const std::exception& e) {
+    std::cerr << "*** Unexpected error: " << e.what() << "\n";
+    return_code = 1;
+  } catch (...) {
+    std::cerr << "*** Unexpected error.\n";
+    return_code = 1;
+  }
+  std::exit(return_code);
 }
 
 [[noreturn]] void print_version_and_exit() {
@@ -47,48 +66,75 @@ namespace {
 }
 
 [[noreturn]] void set_max_size_and_exit(const char* size_arg) {
-  // TODO(m): Implement me!
-  (void)size_arg;
-  std::cout << "*** Setting the max size has not yet implemented\n";
-  std::exit(0);
+  int return_code = 0;
+  try {
+    // TODO(m): Implement me!
+    (void)size_arg;
+    std::cout << "*** Setting the max size has not yet implemented\n";
+  } catch (const std::exception& e) {
+    std::cerr << "*** Unexpected error: " << e.what() << "\n";
+    return_code = 1;
+  } catch (...) {
+    std::cerr << "*** Unexpected error.\n";
+    return_code = 1;
+  }
+  std::exit(return_code);
 }
 
 [[noreturn]] void wrap_compiler_and_exit(int argc, const char** argv) {
   auto args = bcache::string_list_t(argc, argv);
-  if (args.size() < 1) {
-    // Should never happen.
-    std::exit(1);
-  }
-
-  // Find the true path to the executable file. This affects things like if we can match the
-  // compiler name or not, and what version string we get. We also want to avoid incorrectly
-  // identifying other compiler accelerators (e.g. ccache) as actual compilers.
-  const auto true_exe_path = bcache::file::find_executable(args[0]);
-
   bool was_wrapped = false;
-  int return_code = 1;
-  if (!true_exe_path.empty()) {
-    // Initialize a cache object.
-    bcache::cache_t cache;
+  int return_code = 0;
 
-    // Select a matching compiler wrapper.
-    std::unique_ptr<bcache::compiler_wrapper_t> wrapper;
-    if (bcache::gcc_wrapper_t::can_handle_command(true_exe_path)) {
-      wrapper.reset(new bcache::gcc_wrapper_t(cache));
-    } else if (bcache::ghs_wrapper_t::can_handle_command(true_exe_path)) {
-      wrapper.reset(new bcache::ghs_wrapper_t(cache));
+  try {
+    if (args.size() < 1) {
+      // Should never happen.
+      throw std::runtime_error("Missing arguments.");
     }
 
-    // Run the wrapper, if any.
-    if (wrapper) {
-      was_wrapped = wrapper->handle_command(args, true_exe_path, return_code);
+    // Find the true path to the executable file. This affects things like if we can match the
+    // compiler name or not, and what version string we get. We also want to avoid incorrectly
+    // identifying other compiler accelerators (e.g. ccache) as actual compilers.
+    const auto true_exe_path = bcache::file::find_executable(args[0]);
+
+    return_code = 1;
+    if (!true_exe_path.empty()) {
+      // Initialize a cache object.
+      bcache::cache_t cache;
+
+      // Select a matching compiler wrapper.
+      std::unique_ptr<bcache::compiler_wrapper_t> wrapper;
+      if (bcache::gcc_wrapper_t::can_handle_command(true_exe_path)) {
+        wrapper.reset(new bcache::gcc_wrapper_t(cache));
+      } else if (bcache::ghs_wrapper_t::can_handle_command(true_exe_path)) {
+        wrapper.reset(new bcache::ghs_wrapper_t(cache));
+      }
+
+      // Run the wrapper, if any.
+      if (wrapper) {
+        was_wrapped = wrapper->handle_command(args, true_exe_path, return_code);
+      }
     }
+  } catch (const std::exception& e) {
+    bcache::debug::log(bcache::debug::ERROR) << "Unexpected error: " << e.what();
+    return_code = 1;
+  } catch (...) {
+    bcache::debug::log(bcache::debug::ERROR) << "Unexpected error.";
+    return_code = 1;
   }
 
-  // Fall back to running the command as is.
-  if (!was_wrapped) {
-    auto result = bcache::sys::run_with_prefix(args, false);
-    return_code = result.return_code;
+  try {
+    // Fall back to running the command as is.
+    if (!was_wrapped) {
+      auto result = bcache::sys::run_with_prefix(args, false);
+      return_code = result.return_code;
+    }
+  } catch (const std::exception& e) {
+    bcache::debug::log(bcache::debug::FATAL) << "Unexpected error." << e.what();
+    return_code = 1;
+  } catch (...) {
+    bcache::debug::log(bcache::debug::FATAL) << "Unexpected error.";
+    return_code = 1;
   }
 
   std::exit(return_code);
