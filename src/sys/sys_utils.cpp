fed816469dce83a77f8d4b6345515d5aab45874c
m <m@bitsnbites.eu>
2019-03-29 16:52:01 +0100

    sys_utils: Use proper fork/exec with pipes for POSIX
    This enables proper recording of both stdout and stderr on POSIX
    systems.

--- src/sys/sys_utils.cpp
+++ src/sys/sys_utils.cpp
@@ -39,6 +39,11 @@
 #endif
 #include <windows.h>
 #include <vector>
+#else
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #endif
 
 namespace bcache {
@@ -230,34 +235,132 @@ run_result_t run(const string_list_t& args, const bool quiet, const bool redirec
   }
 #endif
 #else
-  // TODO(m): We should use proper fork/exec to get both stdout and stderr etc.
-  auto* fp = popen(cmd.c_str(), "r");
-  if (fp != nullptr) {
-    // Collect stdout until the pipe is closed.
-    const size_t BUF_SIZE = 2048;
-    std::vector<char> buf(BUF_SIZE);
-    while (feof(fp) == 0) {
-      const auto bytes_read = fread(buf.data(), 1, buf.size(), fp);
-      if (bytes_read > 0u) {
-        if (!quiet) {
-          std::cout.write(buf.data(), static_cast<std::streamsize>(bytes_read));
+  // Create pipes for stdout and stderr.
+  int pipe_stdout[2], pipe_stderr[2];
+  if (pipe(pipe_stdout) == -1) {
+    throw std::runtime_error("Error creating stdout pipe.");
+  }
+  if (pipe(pipe_stderr) == -1) {
+    close(pipe_stdout[0]);
+    close(pipe_stdout[1]);
+    throw std::runtime_error("Error creating stderr pipe.");
+  }
+
+  // Create the child process.
+  auto child_pid = fork();
+  if (child_pid == 0) {
+    try {
+      // Redirect stdout & stderr to the pipes.
+      auto dup2_or_throw = [](int fildes, int fildes2) {
+        while (true) {
+          if (dup2(fildes, fildes2) != -1) {
+            break;
+          } else if (errno != EINTR) {
+            throw std::runtime_error("Could not redirect stdout/stderr");
+          }
         }
-        result.std_out += std::string(buf.data(), bytes_read);
+      };
+      dup2_or_throw(pipe_stdout[1], STDOUT_FILENO);
+      dup2_or_throw(pipe_stderr[1], STDERR_FILENO);
+
+      // The child process will not use the pipes directly, so close them.
+      close(pipe_stdout[0]);
+      close(pipe_stdout[1]);
+      close(pipe_stderr[0]);
+      close(pipe_stderr[1]);
+
+      // Build a NULL-terminated argv array from args.
+      std::vector<char*> argv;
+      for (auto& arg : args) {
+        argv.push_back(const_cast<char*>(arg.c_str()));
+      }
+      argv.push_back(nullptr);
+
+      // Call the command.
+      execv(argv[0], &argv[0]);
+
+      // If execv returns, it must have failed.
+      throw std::runtime_error("Unable to launch command via execv()");
+    } catch (std::exception& e) {
+      // TODO(m): Signal this error to the parent process somehow.
+      std::cerr << "*** BuildCache error: " << e.what() << "\n";
+      std::exit(1);
+    }
+  } else if (child_pid != -1) {
+    // The parent has no need for the entrances of the pipes, so close them.
+    close(pipe_stdout[1]);
+    close(pipe_stderr[1]);
+
+    // Read stdout & stderr from the child process.
+    auto read_pipes_failed = false;
+    const size_t BUF_SIZE = 4096;
+    std::vector<char> buf(BUF_SIZE);
+
+    auto read_pipe = [&buf, &read_pipes_failed, quiet](
+                         int fd, std::string& data, std::ostream& out) -> bool {
+      bool has_more_data = true;
+      const auto bytes_read = read(fd, buf.data(), BUF_SIZE);
+      if (bytes_read == -1) {
+        if (errno != EINTR) {
+          debug::log(debug::ERROR)
+              << "Error reading output from child process (errno: " << errno << ")";
+          read_pipes_failed = true;
+        }
+      } else if (bytes_read == 0) {
+        has_more_data = false;
+      } else {
+        if (!quiet) {
+          out.write(buf.data(), static_cast<std::streamsize>(bytes_read));
+        }
+        data += std::string(buf.data(), static_cast<std::string::size_type>(bytes_read));
+      }
+      return has_more_data;
+    };
+
+    // Read stdout, then stderr.
+    while ((!read_pipes_failed) && read_pipe(pipe_stdout[0], result.std_out, std::cout))
+      ;
+    while ((!read_pipes_failed) && read_pipe(pipe_stderr[0], result.std_err, std::cerr))
+      ;
+
+    // We're done with the pipes in the parent process.
+    close(pipe_stdout[0]);
+    close(pipe_stderr[0]);
+
+    // Wait for the child process to terminate.
+    int child_status;
+    while (true) {
+      const auto pid = waitpid(child_pid, &child_status, 0);
+      if (pid == child_pid) {
+        if (WIFEXITED(child_status)) {
+          result.return_code = WEXITSTATUS(child_status);
+        } else if (WIFSIGNALED(child_status)) {
+          result.return_code = WTERMSIG(child_status);
+          debug::log(debug::INFO) << "Child process " << child_pid
+                                  << " terminated (signal: " << WTERMSIG(child_status) << ")";
+        }
+        successfully_launched_program = true;
+        break;
+      } else if (pid == -1 && errno == EINTR) {
+        // Our wait was interrupted. Try again.
+        continue;
+      } else {
+        debug::log(debug::ERROR) << "Unexpected error waiting for pid " << child_pid
+                                 << " (errno: " << errno << ")";
+        result.return_code = 1;
+        break;
       }
     }
 
-    // Close the pipe and get the exit status.
-    const auto status = pclose(fp);
-    if (WIFEXITED(status)) {
-      result.return_code = WEXITSTATUS(status);
-    } else if (WIFSIGNALED(status)) {
-      result.return_code = WTERMSIG(status);
-    } else {
-      result.return_code = 1;
+    // If we didn't get all the stdout/stderr data from the child process, we can't guarantee
+    // correct operation.
+    if (read_pipes_failed) {
+      successfully_launched_program = false;
     }
-    successfully_launched_program = true;
+  } else {
+    debug::log(debug::ERROR) << "Could not create child process (errno: " << errno << ")";
   }
-#endif
+#endif  // _WIN32
 
   if (!successfully_launched_program) {
     throw std::runtime_error("Unable to start the child process.");
