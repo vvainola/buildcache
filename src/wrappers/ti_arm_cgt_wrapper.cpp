06f882cb151da6f4b5e47d764028d439337c1873
m <m@bitsnbites.eu>
2020-03-26 11:58:12 +0100

    Add a wrapper for the TI ARM CGT compiler

---
+++ src/wrappers/ti_arm_cgt_wrapper.cpp
@@ -0,0 +1,42 @@
+//--------------------------------------------------------------------------------------------------
+// Copyright (c) 2020 Marcus Geelnard
+//
+// This software is provided 'as-is', without any express or implied warranty. In no event will the
+// authors be held liable for any damages arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose, including commercial
+// applications, and to alter it and redistribute it freely, subject to the following restrictions:
+//
+//  1. The origin of this software must not be misrepresented; you must not claim that you wrote
+//     the original software. If you use this software in a product, an acknowledgment in the
+//     product documentation would be appreciated but is not required.
+//
+//  2. Altered source versions must be plainly marked as such, and must not be misrepresented as
+//     being the original software.
+//
+//  3. This notice may not be removed or altered from any source distribution.
+//--------------------------------------------------------------------------------------------------
+
+#include <wrappers/ti_arm_cgt_wrapper.hpp>
+
+#include <base/file_utils.hpp>
+#include <base/unicode_utils.hpp>
+
+#include <regex>
+
+namespace bcache {
+ti_arm_cgt_wrapper_t::ti_arm_cgt_wrapper_t(const string_list_t& args) : ti_common_wrapper_t(args) {
+}
+
+bool ti_arm_cgt_wrapper_t::can_handle_command() {
+  // Is this the right compiler?
+  const auto cmd = lower_case(file::get_file_part(m_args[0], true));
+  const std::regex re("armcl.*");
+  if (std::regex_match(cmd, re)) {
+    return true;
+  }
+
+  return false;
+}
+
+}  // namespace bcache
---
+++ src/wrappers/ti_arm_cgt_wrapper.hpp
@@ -0,0 +1,34 @@
+//--------------------------------------------------------------------------------------------------
+// Copyright (c) 2020 Marcus Geelnard
+//
+// This software is provided 'as-is', without any express or implied warranty. In no event will the
+// authors be held liable for any damages arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose, including commercial
+// applications, and to alter it and redistribute it freely, subject to the following restrictions:
+//
+//  1. The origin of this software must not be misrepresented; you must not claim that you wrote
+//     the original software. If you use this software in a product, an acknowledgment in the
+//     product documentation would be appreciated but is not required.
+//
+//  2. Altered source versions must be plainly marked as such, and must not be misrepresented as
+//     being the original software.
+//
+//  3. This notice may not be removed or altered from any source distribution.
+//--------------------------------------------------------------------------------------------------
+
+#ifndef BUILDCACHE_TI_ARM_CGT_HPP_
+#define BUILDCACHE_TI_ARM_CGT_HPP_
+
+#include <wrappers/ti_common_wrapper.hpp>
+
+namespace bcache {
+/// @brief A program wrapper for the TI ARM CGT compiler.
+class ti_arm_cgt_wrapper_t : public ti_common_wrapper_t {
+public:
+  ti_arm_cgt_wrapper_t(const string_list_t& args);
+  bool can_handle_command() override;
+};
+}  // namespace bcache
+
+#endif  // BUILDCACHE_TI_ARM_CGT_HPP_
