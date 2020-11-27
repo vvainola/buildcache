c288f9afe8ae91c311dcaad25ac976471d621abd
m <m@bitsnbites.eu>
2020-11-27 13:30:10 +0100

    Add a clang-format check to run_linters.py
    This closes #100

--- test_scripts/run_linters.py
+++ test_scripts/run_linters.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import difflib
 import subprocess
 import sys
 from multiprocessing.pool import ThreadPool
@@ -39,6 +40,47 @@ def clang_tidy(src, build_path, fix):
     return proc.stdout
 
 
+def read_file(file_name):
+    with open(file_name, encoding="utf8") as f:
+        return f.readlines()
+
+
+def files_equal(a, b):
+    if len(a) != len(b):
+        return False
+    for i in range(0, len(a)):
+        if a[i] != b[i]:
+            return False
+    return True
+
+
+def clang_format(src, fix):
+    file_name = src.relative_to(_SRC_DIR)
+    print(f"clang-format: {file_name}")
+    args = ["clang-format", "-Werror"]
+    if fix:
+        args.append("-i")
+    else:
+        args.append("--dry-run")
+    args.append(src)
+    before = read_file(src)
+    proc = subprocess.run(
+        args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8"
+    )
+    after = read_file(src)
+    result = proc.stderr
+    if not files_equal(after, before):
+        if result:
+            result += "\n"
+        result += f"{src}: warning: code was clang-formatted:\n"
+        result += "".join(
+            difflib.context_diff(
+                before, after, fromfile=str(file_name), tofile=str(file_name), n=0
+            )
+        )
+    return result
+
+
 def run_clang_tidy(build_path, fix):
     sources = collect_sources(".cpp")
 
@@ -64,8 +106,35 @@ def run_clang_tidy(build_path, fix):
     return [r.get() for r in results if r.get()]
 
 
+def run_clang_format(fix):
+    sources = collect_sources(".cpp")
+    sources.extend(collect_sources(".hpp"))
+
+    # Run jobs in a thread pool.
+    pool = ThreadPool()
+    results = []
+    for src in sources:
+        results.append(
+            pool.apply_async(
+                clang_format,
+                [
+                    src,
+                    fix,
+                ],
+            )
+        )
+
+    # Wait for all workers to finish.
+    pool.close()
+    pool.join()
+
+    return [r.get() for r in results if r.get()]
+
+
 def run_linters(build_path, fix):
-    errors = run_clang_tidy(build_path, fix)
+    errors = []
+    errors.extend(run_clang_tidy(build_path, fix))
+    errors.extend(run_clang_format(fix))
 
     all_ok = True
     if errors:
