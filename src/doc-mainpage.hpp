e95145c9860fe5d4aeb982c80f2c73d1f66a8cad
m <m@bitsnbites.eu>
2021-02-23 13:34:41 +0100

    doxygen: Add documentation of namespaces

--- src/doc-mainpage.hpp
+++ src/doc-mainpage.hpp
@@ -31,6 +31,11 @@
 /// This documentation describes the BuildCache internals. It is mainly useful for BuildCache
 /// contributors.
 ///
+/// @section wrapper_sec Creating a new program wrapper
+///
+/// In order to add support for a new compiler or program, create a new class that inherits from the
+/// @ref bcache::program_wrapper_t class.
+///
 /// @section structur_sec Code structure
 ///
 /// The BuildCache source code is divided into the following parts:
@@ -40,5 +45,51 @@
 ///   - wrappers - The wrappers for different compilers and programs.
 ///   - config - Program configuration logic.
 ///   - third_party - Third party libraries.
+///
+/// @section namespaces_sec Namespaces
+///
+/// The following namespaces are used in BuildCache:
+///   - @ref bcache
+///   - @ref bcache::comp
+///   - @ref bcache::config
+///   - @ref bcache::debug
+///   - @ref bcache::file
+///   - @ref bcache::perf
+///   - @ref bcache::serialize
+///   - @ref bcache::sys
+///   - @ref bcache::time
+
+/// @namespace bcache
+/// @brief The top level namespace.
+///
+/// All BuildCache functionality lives in the @c bcache namespace or one of its decendant
+/// namespaces.
+
+/// @namespace bcache::config
+/// @brief BuildCachce configuration options.
+///
+/// Configuration options are gathered during program startup, and they can be queried via the
+/// functions in the @c config namespace (which acts as a sigleton).
+
+/// @namespace bcache::comp
+/// @brief Compression functions.
+
+/// @namespace bcache::debug
+/// @brief Debug logging functions.
+
+/// @namespace bcache::file
+/// @brief File utility functions and classes.
+
+/// @namespace bcache::perf
+/// @brief Performance profiling functions.
+
+/// @namespace bcache::serialize
+/// @brief Data (de)serialization functions.
+
+/// @namespace bcache::sys
+/// @brief System utility functions.
+
+/// @namespace bcache::time
+/// @brief Portable file time types and functions.
 
 #endif  // BUILDCACHE_DOC_MAINPAGE_HPP_
