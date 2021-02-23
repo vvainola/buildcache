07a12887920a581799a6dfe8d4459e5446956aed
m <m@bitsnbites.eu>
2021-02-23 12:22:40 +0100

    doxygen: Add a mainpage

---
+++ src/doc-mainpage.hpp
@@ -0,0 +1,44 @@
+//--------------------------------------------------------------------------------------------------
+// Copyright (c) 2021 Marcus Geelnard
+//
+// This software is provided 'as-is', without any express or implied warranty. In no event will the
+// authors be held liable for any damages arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose, including commercial
+// applications, and to alter it and redistribute it freely, subject to the following restrictions:
+//
+//  1. The origin of this software must not be misrepresented; you must not claim that you wrote
+//     the original software. If you use this software in a product, an acknowledgment in the
+//     product documentation would be appreciated but is not required.
+//
+//  2. Altered source versions must be plainly marked as such, and must not be misrepresented as
+//     being the original software.
+//
+//  3. This notice may not be removed or altered from any source distribution.
+//--------------------------------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------------------------------
+// Purpose:  This is the Doxygen main page. The file is not actually used by the BuildCache source.
+//--------------------------------------------------------------------------------------------------
+
+#ifndef BUILDCACHE_DOC_MAINPAGE_HPP_
+#define BUILDCACHE_DOC_MAINPAGE_HPP_
+
+/// @mainpage BuildCache internals documentation
+///
+/// @section intro_sec Introduction
+///
+/// This documentation describes the BuildCache internals. It is mainly useful for BuildCache
+/// contributors.
+///
+/// @section structur_sec Code structure
+///
+/// The BuildCache source code is divided into the following parts:
+///   - base - A portable set of support classes and functions.
+///   - sys - System specific support functions.
+///   - cache - The implementation of the cache logic (wrapper independent).
+///   - wrappers - The wrappers for different compilers and programs.
+///   - config - Program configuration logic.
+///   - third_party - Third party libraries.
+
+#endif  // BUILDCACHE_DOC_MAINPAGE_HPP_
